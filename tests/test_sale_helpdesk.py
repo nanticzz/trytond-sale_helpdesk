d8ddbbe36b945f8a5abb5dd41eb03e4fc8c54dbd
jmartin <jmartin@zikzakmedia.com>
2014-11-07 14:37:51 +0100

    Cleanup tests

--- tests/test_sale_helpdesk.py
+++ tests/test_sale_helpdesk.py
@@ -2,37 +2,23 @@
 # This file is part of the sale_helpdesk module for Tryton.
 # The COPYRIGHT file at the top level of this repository contains the full
 # copyright notices and license terms.
-
-import sys
-import os
-DIR = os.path.abspath(os.path.normpath(os.path.join(__file__,
-    '..', '..', '..', '..', '..', 'trytond')))
-if os.path.isdir(DIR):
-    sys.path.insert(0, os.path.dirname(DIR))
-
 import unittest
 import trytond.tests.test_tryton
 from trytond.tests.test_tryton import test_view, test_depends
 
 
 class SaleHelpdeskTestCase(unittest.TestCase):
-    '''
-    Test Sale Helpdesk module.
-    '''
+    'Test Sale Helpdesk module'
 
     def setUp(self):
         trytond.tests.test_tryton.install_module('sale_helpdesk')
 
     def test0005views(self):
-        '''
-        Test views.
-        '''
+        'Test views'
         test_view('sale_helpdesk')
 
     def test0006depends(self):
-        '''
-        Test depends.
-        '''
+        'Test depends'
         test_depends()
 
 
