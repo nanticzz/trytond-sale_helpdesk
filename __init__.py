06e4d71da99edd917f41049370d27db58ac98973
jmartin <jmartin@zikzakmedia.com>
2015-10-22 11:59:38 +0200

    Add configuration capability

--- __init__.py
+++ __init__.py
@@ -2,11 +2,13 @@
 # The COPYRIGHT file at the top level of this repository contains the full
 # copyright notices and license terms.
 from trytond.pool import Pool
+from .configuration import *
 from .helpdesk import *
 
 
 def register():
     Pool.register(
+        HelpdeskConfiguration,
         Helpdesk,
         SaleHelpdesk,
         module='sale_helpdesk', type_='model')
---
+++ configuration.py
@@ -0,0 +1,13 @@
+# This file is part sale_helpdesk module for Tryton.
+# The COPYRIGHT file at the top level of this repository contains
+# the full copyright notices and license terms.
+from trytond.model import fields
+from trytond.pool import PoolMeta
+
+__all__ = ['HelpdeskConfiguration']
+__metaclass__ = PoolMeta
+
+
+class HelpdeskConfiguration:
+    __name__ = 'helpdesk.configuration'
+    smtp_sale = fields.Many2One('smtp.server', 'SMTP Sales Server')
