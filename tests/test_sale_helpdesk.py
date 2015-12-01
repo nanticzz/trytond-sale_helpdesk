ee31824a78400154ee8cffb57ff4c7411acec306
resteve <resteve@zikzakmedia.com>
2015-12-01 11:19:05 +0100

    Tests

--- tests/test_sale_helpdesk.py
+++ tests/test_sale_helpdesk.py
@@ -1,25 +1,14 @@
-#!/usr/bin/env python
 # This file is part of the sale_helpdesk module for Tryton.
 # The COPYRIGHT file at the top level of this repository contains the full
 # copyright notices and license terms.
 import unittest
 import trytond.tests.test_tryton
-from trytond.tests.test_tryton import test_view, test_depends
+from trytond.tests.test_tryton import ModuleTestCase
 
 
-class SaleHelpdeskTestCase(unittest.TestCase):
+class SaleHelpdeskTestCase(ModuleTestCase):
     'Test Sale Helpdesk module'
-
-    def setUp(self):
-        trytond.tests.test_tryton.install_module('sale_helpdesk')
-
-    def test0005views(self):
-        'Test views'
-        test_view('sale_helpdesk')
-
-    def test0006depends(self):
-        'Test depends'
-        test_depends()
+    module = 'sale_helpdesk'
 
 
 def suite():
@@ -27,6 +16,3 @@ def suite():
     suite.addTests(unittest.TestLoader().loadTestsFromTestCase(
         SaleHelpdeskTestCase))
     return suite
-
-if __name__ == '__main__':
-    unittest.TextTestRunner(verbosity=2).run(suite())
